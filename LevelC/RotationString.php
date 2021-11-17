01758c22247686a03de73eb9b51d7d1837245d3f
eldormero <martin.coulon@epita.fr>
2021-11-17 21:09:38 +0100

    Add exercice C

--- LevelC/RotationString.php
+++ LevelC/RotationString.php
@@ -15,8 +15,12 @@ class RotationString
     public static function isRotation($s1, $s2)
     {
         /** @TODO */
-
-        return false;
+        $i = 0;
+        while ($i < strlen($s1) && $s1 != $s2) {
+            $s1 = substr($s1, 1) . $s1[0];
+            $i++;
+        }
+        return $i != strlen($s1) ? true : false;
     }
 
     public static function isSubString($s1, $s2)
